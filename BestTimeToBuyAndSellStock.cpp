112ea41a9a01ad536ada8011ba34efab7ae0bd0e
GeWu <529852811@qq.com>
2015-04-01 08:45:03 +0800

    new questions

--- BestTimeToBuyAndSellStock.cpp
+++ BestTimeToBuyAndSellStock.cpp
@@ -15,14 +15,39 @@
  **/
 
 #include <iostream>
+#include <vector>
 using namespace std;
 
 class Solution{
 public:
     int maxProfit(vector<int> &prices){
-        
+        int low = prices[0];
+        int max = 0;
+        int profit = 0;
+        for (int i = 1; i < prices.size(); i++){
+            profit = prices[i] - low;
+            if (profit > max)
+                max = profit;
+            if (low > prices[i])
+                low = prices[i];
+        }
+        return max;        
     }
 };
+
+int main(){
+    vector<int> A ;
+    A.push_back(2);
+    A.push_back(1);
+    A.push_back(4);
+    A.push_back(5);
+    A.push_back(0);
+    A.push_back(9);
+    Solution solution;
+    int val = solution.maxProfit(A);
+    cout << val << endl;
+
+}
 
 
 
